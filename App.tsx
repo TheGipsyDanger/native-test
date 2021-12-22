a92bc15fc4e96bd7ca35aeb290bb5e820c8fa4c1
Renan Henrique <henriquebasshvf@gmail.com>
2021-12-22 16:49:07 -0300

    chore: add react-query on project and add a initial config on API

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { Store } from './src/components';
 import Routes from './src/routes/Routes';
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => (
-  <NavigationContainer>
-    <Store>
-      <Routes />
-    </Store>
-  </NavigationContainer>
+  <QueryClientProvider client={queryClient}>
+    <NavigationContainer>
+      <Store>
+        <Routes />
+      </Store>
+    </NavigationContainer>
+  </QueryClientProvider>
 );
 
 export default App;
